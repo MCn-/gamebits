973974a866ca1f8d50de540cea78597f4198708a
MCn- <MCn-@users.noreply.github.com>
2015-02-13 03:22:37 +0000

    Update code a bit + disable search for now

--- README.md
+++ README.md
@@ -7,13 +7,10 @@ Installation
 ============
 
 - Download the files from https://github.com/MCn-/gamebits/archive/mGameBits.zip
-- Install pip: http://www.pip-installer.org/en/latest/installing.html
-    - If you're not sure how to do this, install setuptools (http://pythonhosted.org/setuptools/easy_install.html#installing-easy-install) and run ``easy_install pip`` on the command line.
-    - If you're really struggling, have a look at https://sites.google.com/site/pydatalog/python/pip-for-windows (not used myself, but seems promising)
-    - If easy_install or pip aren't being found, try adding the python folders to your path: http://www.anthonydebarros.com/2011/10/15/setting-up-python-in-windows-7/ **point 3**
+- Install python 2, which should come with pip
 - (Optional: use a virtualenv)
 - Once pip is installed, ``cd`` to the gamebits/ directory (that you just downloaded from github!) and run ``pip install -r requirements.txt``. This will download all dependencies automatically.
-- Put gamebits.py in a folder somewhere, optionally add it to your path
+- Put mGameBits.py in a folder somewhere, optionally add it to your path
 
 Usage
 ========
@@ -24,7 +21,7 @@ You can run the script by supplying either a URL to a MobyGames game page or sim
 
 or
 
-    ./mGameBits.py <MG URL> <CONSOLE> <SOURCE> <LANGUAGE>
-    
+    ./mGameBits.py <MG URL> [<CONSOLE>] [<SOURCE>] [<LANGUAGE>]
+
 Note: CONSOLE should be equal to the one used on MG, which usually will be fairly similar to what you would expect, but in some cases may be different. For example, the Nintendo 64 has an id of n64. I recommend just trying it and seeing what happens: it will probably work in 99% of cases without much thought.
-    
+
--- mGameBits.py
+++ mGameBits.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 # Really early initial implementation of game info grabber - there will be tons of bugs, beware
 # MCn
 
-from bs4 import BeautifulSoup
+from pyquery import PyQuery as pq
+import argparse
 import requests
-import html5lib
-import sys, re, json
+import sys
+import re
+import json
 
 # Codes used in search urls on MobyGames - add more if necessary
 MG_CONSOLE_CODES = {'pc': 3, 'gb': 10, 'gbc': 11, 'gba': 12}
@@ -16,184 +19,206 @@ MG_CONSOLE_SLUGS = {'N64': 'N64', 'GBA': 'gameboy-advance', 'GBC': 'gameboy-colo
 def upload_image(url):
     try:
         # dealing with pesky relative url
-        if not 'http://' in url:
+        if 'http://' not in url:
             url = 'https://www.mobygames.com/' + url
         # Check for weird url bug where sometimes the image would have two http://
-        if '/https://' or '/http://' in url:
+        if '/https://' in url or '/http://' in url:
             url = url[len('https://www.mobygames.com/'):]
         # Remove double slashes
         url = url.replace('com//', 'com/')
         headers = {"Authorization": "Client-ID d656b9b04c8ff24"}
         params = {"image": url}
-        r = requests.post("https://api.imgur.com/3/image", headers = headers, params = params)
+        r = requests.post("https://api.imgur.com/3/image", headers=headers, params=params)
         return json.loads(r.content)["data"]["link"]
     except:
         return ''
 
-# Get URL if none supplied
-if "http" in sys.argv[1]:
-    game_page = sys.argv[1]
-    game_name = ""
-else:
-    game_name = sys.argv[1]
 
-    # Remove spaces and lowercase console name
-    console = '-'.join(sys.argv[2].split()).lower()
+# def get_game_page():
+#     if "http" in sys.argv[1]:
+#         return sys.argv[1]
+#         # game_name = ""
+#     else:
+#         print 'Search is currently not enabled, it needs to be fixed. Try passing in a URL.'
+#         return None
+        # game_name = sys.argv[1]
 
-    url_friendly_gn = ''.join(e for e in game_name.lower() if e.isalnum() or e == ' ').replace(' ', '-')
+        # # Remove spaces and lowercase console name
+        # console = '-'.join(args.console.split()).lower()
 
-    # Try naive search with console
-    if console in MG_CONSOLE_SLUGS:
-        console_slug = format(MG_CONSOLE_SLUGS['console'])
+        # url_friendly_gn = ''.join(e for e in game_name.lower() if e.isalnum() or e == ' ').replace(' ', '-')
+
+        # # Try naive search with console
+        # if console in MG_CONSOLE_SLUGS:
+        #     console_slug = format(MG_CONSOLE_SLUGS['console'])
+        # else:
+        #     console_slug = console
+        # game_page = 'https://www.mobygames.com/game/{0}/{1}'.format(console_slug, url_friendly_gn)
+        # naive_soup = BeautifulSoup(requests.get(game_page).text, 'html5lib')
+
+        # if not naive_soup.find('h1', class_='gameHeader'):
+        #     # Try naive search without console
+        #     game_page = 'https://www.mobygames.com/game/{0}'.format(url_friendly_gn)
+        #     naive_soup = BeautifulSoup(requests.get(game_page).text.encode('utf-8'), 'html5lib')
+
+        #     if not naive_soup.find('h1', class_='gameHeader'):
+        #         # NEED TO SEARCH :(
+        #         if console in MG_CONSOLE_CODES:
+        #             console_code = MG_CONSOLE_CODES[console]
+        #         else:
+        #             console_code = -1
+
+        #         # Search MobyGames for games
+        #         search_url = 'https://www.mobygames.com/search/quick?q={0}&p={1}&search=Go&sFilter=1&sG=on'.format(game_name, console_code)
+        #         search_soup = BeautifulSoup(requests.get(search_url).text)
+
+        #         # # Find the page for the game
+        #         game_page = search_soup.find("div", class_="searchResult").a.get('href')
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("url", help="url of a mobygames game page")
+    parser.add_argument("--source", help="source/format of the game")
+    parser.add_argument("--language", help="language of the game")
+    parser.add_argument("--console", help="the console this game plays on - used for emulator suggestion information", choices=["PS1", "PS2", "NES", "SNES", "N64", "GB", "GBC", "GBA", "GC", "WII", "DS", "DOS"])
+    args = parser.parse_args()
+
+    if 'http' in args.url:
+        game_page = args.url
     else:
-        console_slug = console
-    game_page = 'https://www.mobygames.com/game/{0}/{1}'.format(console_slug, url_friendly_gn)
-    naive_soup = BeautifulSoup(requests.get(game_page).text.encode('utf-8'), 'html5lib')
+        print 'Search is currently not enabled, it needs to be fixed. Try passing in a URL.'
+        return
 
-    if not naive_soup.find('h1', class_='gameHeader'):
-        # Try naive search without console
-        game_page = 'https://www.mobygames.com/game/{0}'.format(url_friendly_gn)
-        naive_soup = BeautifulSoup(requests.get(game_page).text.encode('utf-8'), 'html5lib')
+    page = pq(url=game_page).make_links_absolute()
 
-        if not naive_soup.find('h1', class_='gameHeader'):
-            # NEED TO SEARCH :(
-            if console in MG_CONSOLE_CODES:
-                console_code = MG_CONSOLE_CODES[console]
-            else:
-                console_code = -1
+    # display_strings = {
+    #     'source':
+    # }
 
-            # Search MobyGames for games
-            search_url = 'https://www.mobygames.com/search/quick?q={0}&p={1}&search=Go&sFilter=1&sG=on'.format(game_name, console_code)
-            search_soup = BeautifulSoup(requests.get(search_url).text)
+    # # Source and language args
+    # try:
+    #     source = sys.argv[3]
+    # except:
+    #     source = None
 
-            # # Find the page for the game
-            game_page = search_soup.find("div", class_="searchResult").a.get('href')
+    # try:
+    #     language = sys.argv[4]
+    # except:
+    #     language = None
 
-# Open URL, need html5lib since default parser breaks on mobygames
-soup = BeautifulSoup(requests.get(game_page).text, "html5lib")
+    proper_game_title = page('.niceHeaderTitle a').eq(0).text()
 
-# Source and language args
-try:
-    source = sys.argv[3]
-except:
-    source = None
+    date = page('#coreGameRelease a[href*="release-info"]').text()
 
-try:
-    language = sys.argv[4]
-except:
-    language = None
+    # NOTE: u'\xa0' is &nbsp; - replace it with a space
+    genre = page('#coreGameGenre a[href*="genre"]').eq(0).text().replace(u'\xa0', ' ')
 
-proper_game_title = soup.find('div', id='gameTitle').get_text()
+    review = game_page + '/mobyrank'
 
-game_release_div = soup.find('div', id='coreGameRelease')
-date = game_release_div.find('div', text='Released').next_sibling.get_text()
+    page = pq(url='http://www.mobygames.com/game/windows/dragon-age-origins')
+    dirty_description_text = pq(page('h2:contains("Description")').parent().html().replace('<br/>', '\n')).text()
+    description_text = re.search(r'Description\s(.*)\s\[ edit', dirty_description_text, re.DOTALL).group(1)
 
-game_genre_div = soup.find('div', id='coreGameGenre')
-# NOTE: u'\xa0' is &nbsp; - remove it
-genre = game_genre_div.find('div', text='Genre').next_sibling.get_text().replace(u'\xa0', '')
+    # Get box art
+    game_box_data = page('#coreGameCover img')
+    if game_box_data:
+        img_url = game_box_data.attr('src').replace('small', 'large')
+        imgur_game_box_url = upload_image(img_url)
+    else:
+        imgur_game_box_url = None
 
-review = game_page + '/mobyrank'
+    # Get screenshots
+    screenshot_gallery_url = game_page + '/screenshots'
+    screenshot_page = pq(url=screenshot_gallery_url).make_links_absolute()
+    screenshot_images = screenshot_page('.mobythumbnail img')
+    screenshot_one_url, screenshot_two_url = None, None
+    if len(screenshot_images) > 0:
+        screenshot_one_url = upload_image(screenshot_images.eq(0).attr('src').replace('/s/', '/l/'))
+    if len(screenshot_images) > 1:
+        screenshot_two_url = upload_image(screenshot_images.eq(1).attr('src').replace('/s/', '/l/'))
 
-description_div = soup.find('div', class_='rightPanelMain')
-for br in description_div.findAll('br'):
-    br.replace_with('\n')
-string = description_div.get_text().encode('utf-8')
-description_text = string[len('Description'):string.find('[edit description')]
+    # Print everything
+    print "Game Name: " + proper_game_title
+    print "Released: " + date
+    if args.source:
+        print "Source: " + args.source
+    if args.language:
+        print "Language: " + args.language
+    print "Game Genre: " + genre + "\n"
 
-# Get box art
-game_box_data = soup.find("div", id="coreGameCover").img
-if game_box_data:
-    img_url = game_box_data.get("src").encode('utf-8').replace('small', 'large')
-    imgur_game_box_url = upload_image(img_url)
-else:
-    imgur_game_box_url = None
+    print "[b]Review:[/b] " + review + "\n"
 
-# Get screenshots
-screenshot_gallery_url = game_page + '/screenshots'
-screenshot_soup = BeautifulSoup(requests.get(screenshot_gallery_url).text, "html5lib")
-screenshot_images = screenshot_soup.find('div', class_='rightPanelMain').findAll('img')
-screenshot_one_url, screenshot_two_url = None, None
-if len(screenshot_images) > 0:
-    screenshot_one_url = upload_image(screenshot_images[0].get('src').replace('/s/', '/l/'))
-if len(screenshot_images) > 1:
-    screenshot_two_url = upload_image(screenshot_images[1].get('src').replace('/s/', '/l/'))
+    print "[b]Description:[/b] "
+    print "[quote]"
+    print description_text
+    print "[/quote]"
+    print ""
 
-# Print everything
-print "Game Name: " + proper_game_title.encode('utf-8')
-print "Released: " + date.encode('utf-8')
-if source:
-    print "Source: " + source.encode('utf-8')
-if language:
-    print "Language: " + language.encode('utf-8')
-print "Game Genre: " + genre.encode('utf-8') + "\n"
+    if len(sys.argv) > 2:
+        # Emulator Suggestion
+        if args.console == "PS1":
+            print "[b]Emulation:[/b]"
+            print "[quote]The best Emulator to use is EPSXE."
+            print "http://www.epsxe.com/download.php[/quote]"
+        elif args.console == "PS2":
+            print "[b]Emulation:[/b]"
+            print "[quote]The best Emulator to use is PCSX2."
+            print "http://pcsx2.net/download.html[/quote]"
+        elif args.console == "NES":
+            print "[b]Emulation:[/b]"
+            print "[quote]The best Emulator to use is FCEUX."
+            print "http://www.fceux.com/web/home.html[/quote]"
+        elif args.console == "SNES":
+            print "[b]Emulation:[/b]"
+            print "[quote]The best Emulator to use is ZSNES."
+            print "http://www.zsnes.com/index.php?page=files[/quote]"
+        elif args.console == "N64":
+            print "[b]Emulation:[/b]"
+            print "[quote]The best Emulator to use is Project 64."
+            print "http://www.pj64-emu.com/[/quote]"
+        elif args.console == "GB":
+            print "[b]Emulation:[/b]"
+            print "[quote]The best Emulator to use is Virtual Boy Advanced."
+            print "http://vba.ngemu.com/downloads.shtml[/quote]"
+        elif args.console == "GBC":
+            print "[b]Emulation:[/b]"
+            print "[quote]The best Emulator to use is Virtual Boy Advanced."
+            print "http://vba.ngemu.com/downloads.shtml[/quote]"
+        elif args.console == "GBA":
+            print "[b]Emulation:[/b]"
+            print "[quote]The best Emulator to use is Virtual Boy Advanced."
+            print "http://vba.ngemu.com/downloads.shtml[/quote]"
+        elif args.console == "GC":
+            print "[b]Emulation:[/b]"
+            print "[quote]The best Emulator to use is Dolphin."
+            print "http://www.dolphin-emulator.com/download.html[/quote]"
+        elif args.console == "WII":
+            print "[b]Emulation:[/b]"
+            print "[quote]The best Emulator to use is Dolphin."
+            print "http://www.dolphin-emulator.com/download.html[/quote]"
+        elif args.console == "DS":
+            print "[b]Emulation:[/b]"
+            print "[quote]The best Emulator to use is DSEmu."
+            print "http://dsemu.oopsilon.com/[/quote]"
+        elif args.console == "DOS":
+            print "[b]Emulation:[/b]"
+            print "[quote]The best Emulator to use is DOSBox."
+            print "http://www.dosbox.com/download.php?main=1[/quote]"
 
-print "[b]Review:[/b] " + review.encode('utf-8') + "\n"
+    if screenshot_one_url:
+        print "\n[b]Screenshots:[/b]\n"
+        print "[img]{0}[/img]".format(screenshot_one_url)
+    if screenshot_two_url:
+        print "[img]{0}[/img]".format(screenshot_two_url)
+    if screenshot_one_url:
+        print "Screenshot gallery: " + screenshot_gallery_url
 
-print "[b]Description:[/b] "
-print "[quote]"
-print description_text
-print "[/quote]"
-print ""
+    if imgur_game_box_url:
+        print "\n\nCover image: " + imgur_game_box_url
 
-# Emulator Suggestion
-if sys.argv[2] == "PS1":
-    print "[b]Emulation:[/b]"
-    print "[quote]The best Emulator to use is EPSXE."
-    print "http://www.epsxe.com/download.php[/quote]"
-elif sys.argv[2] == "PS2":
-    print "[b]Emulation:[/b]"
-    print "[quote]The best Emulator to use is PCSX2."
-    print "http://pcsx2.net/download.html[/quote]"
-elif sys.argv[2] == "NES":
-    print "[b]Emulation:[/b]"
-    print "[quote]The best Emulator to use is FCEUX."
-    print "http://www.fceux.com/web/home.html[/quote]"
-elif sys.argv[2] == "SNES":
-    print "[b]Emulation:[/b]"
-    print "[quote]The best Emulator to use is ZSNES."
-    print "http://www.zsnes.com/index.php?page=files[/quote]"
-elif sys.argv[2] == "N64":
-    print "[b]Emulation:[/b]"
-    print "[quote]The best Emulator to use is Project 64."
-    print "http://www.pj64-emu.com/[/quote]"
-elif sys.argv[2] == "GB":
-    print "[b]Emulation:[/b]"
-    print "[quote]The best Emulator to use is Virtual Boy Advanced."
-    print "http://vba.ngemu.com/downloads.shtml[/quote]"
-elif sys.argv[2] == "GBC":
-    print "[b]Emulation:[/b]"
-    print "[quote]The best Emulator to use is Virtual Boy Advanced."
-    print "http://vba.ngemu.com/downloads.shtml[/quote]"
-elif sys.argv[2] == "GBA":
-    print "[b]Emulation:[/b]"
-    print "[quote]The best Emulator to use is Virtual Boy Advanced."
-    print "http://vba.ngemu.com/downloads.shtml[/quote]"
-elif sys.argv[2] == "GC":
-    print "[b]Emulation:[/b]"
-    print "[quote]The best Emulator to use is Dolphin."
-    print "http://www.dolphin-emulator.com/download.html[/quote]"
-elif sys.argv[2] == "WII":
-    print "[b]Emulation:[/b]"
-    print "[quote]The best Emulator to use is Dolphin."
-    print "http://www.dolphin-emulator.com/download.html[/quote]"
-elif sys.argv[2] == "DS":
-    print "[b]Emulation:[/b]"
-    print "[quote]The best Emulator to use is DSEmu."
-    print "http://dsemu.oopsilon.com/[/quote]"
-elif sys.argv[2] == "DOS":
-    print "[b]Emulation:[/b]"
-    print "[quote]The best Emulator to use is DOSBox."
-    print "http://www.dosbox.com/download.php?main=1[/quote]"
+    print "\n-------------------------------------------------\n"
 
-if screenshot_one_url:
-    print "\n[b]Screenshots:[/b]\n"
-    print "[img]{0}[/img]".format(screenshot_one_url)
-if screenshot_two_url:
-    print "[img]{0}[/img]".format(screenshot_two_url)
-if screenshot_one_url:
-    print "Screenshot gallery: " + screenshot_gallery_url
 
-if imgur_game_box_url:
-    print "\n\nCover image: " + imgur_game_box_url
-
-print "\n-------------------------------------------------\n"
+if __name__ == '__main__':
+    main()
